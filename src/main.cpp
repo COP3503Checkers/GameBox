581b690231df5f12261bdcfd73e30f755a5c39ab
donovankryak <thedonovankane@gmail.com>
2016-04-06 21:24:41 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,22 @@
+using namespace std;
+#include <cmath>
+#include <iostream>
+
+#define VACANT 1
+#define RED 2
+#define BLACK 3
+#define RKING 4
+#define BKING 5
+
+#define
+#define
+
+#define ROW 10
+#define COL 10
+
+
 int main(int argc, char *argv[]) {
 	//TODO
+	#define
 	return 0;
 }
